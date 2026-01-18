cd9f12fa9d96a976b8bd8b47f25b03ff5a151b52
dimalavrenuk <105659427+dimalavrenuk@users.noreply.github.com>
2026-01-18 22:30:40 +0200

    Add AttackPreservation contract

---
+++ src/16 - attackPreservation.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract AttackPreservation {
+    address public timeZone1Library; 
+    address public timeZone2Library; 
+    address public owner;            
+    uint storedTime;                 
+
+    function setTime(uint256 _time) public {
+        owner = msg.sender;
+    }
+}
